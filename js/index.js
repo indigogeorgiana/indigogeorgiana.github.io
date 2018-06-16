2e31a032d515d2b293ffcc280be90dfc76c1b06c
Indigo Georgiana <indigogeorgiana@gmail.com>
2018-06-16 18:30:45 +1200

    Create hbs files for blogs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+module.exports = {
+  myFunction
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction () {
+  document.getElementById('myDropdown').classList.toggle('show')
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName('dropdown-content')
+    let i
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show')
+      }
+    }
+  }
+}
+
 // var box = document.getElementById('display');
 // var num = box.value;
 // var len = num.length-1;
@@ -5,7 +29,7 @@
 // //CREATE A FUNCTION TO ADD INPUT TO SCREEN
 // function addToInput(x) {
 //    box.value += x;
-// //IF CLEAR IS SELECTED, CLEAR ALL INPUT ON SCREEN   
+// //IF CLEAR IS SELECTED, CLEAR ALL INPUT ON SCREEN
 //    if (x === 'c'){
 //    box.value = '';
 // }
